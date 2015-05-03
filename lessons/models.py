ddff7a97f957fef61d5963be91cb62d09b7c6643
Shannon Tsaltas <shannontsaltas@gmail.com>
2015-05-03 18:46:41 -0500

    converted tabs to spaces in models.py

--- lessons/models.py
+++ lessons/models.py
@@ -4,54 +4,52 @@ from django.db import models
 
 
 class Activity(models.Model):
-	"""
-	An activity is a collection of steps
-	"""
+    """
+    An activity is a collection of steps
+    """
 
-	# REQUIRED
-	title = models.CharField(max_length=100, unique=True)
-	language = models.CharField(max_length=15)
-	votecount = models.IntegerField(default=0)
+    # REQUIRED
+    title = models.CharField(max_length=100, unique=True)
+    language = models.CharField(max_length=15)
+    votecount = models.IntegerField(default=0)
 
-	#OPTIONAL
-	level = models.CharField(max_length=10, blank=True, null=True)
+    # OPTIONAL
+    level = models.CharField(max_length=10, blank=True, null=True)
 
-	# activities have a collection of steps - access through Activity.steps
-	def __unicode__(self):
-		return self.title
+    # activities have a collection of steps - access through Activity.steps
+    def __unicode__(self):
+        return self.title
 
-	class Meta:
-		verbose_name_plural = "Activities"
-		
+    class Meta:
+        verbose_name_plural = "Activities"
+
 
 class Step(models.Model):
-	"""
-	A step is the atomic unit of instruction
-	"""
+    """
+    A step is the atomic unit of instruction
+    """
 
-	# REQUIRED
-	description = models.CharField(max_length=100)
-	sequence = models.IntegerField()
-	activity = models.ForeignKey(Activity, related_name='steps')
+    # REQUIRED
+    description = models.CharField(max_length=100)
+    sequence = models.IntegerField()
+    activity = models.ForeignKey(Activity, related_name='steps')
 
-	# OPTIONAL
-	teachingnote = models.CharField(max_length=100, blank=True, null=True)
-	imagepath = models.ImageField(upload_to="step-images", blank=True, null=True)
+    # OPTIONAL
+    teachingnote = models.CharField(max_length=100, blank=True, null=True)
+    imagepath = models.ImageField(upload_to="step-images", blank=True, null=True)
 
-	def __unicode__(self):
-		return self.description
+    def __unicode__(self):
+        return self.description
+
 
 class Tag(models.Model):
-	"""
-	A tag is applied to an activity
-	"""
+    """
+    A tag is applied to an activity
+    """
 
-	# REQUIRED
-	description = models.CharField(max_length=20)
-	activity = models.ForeignKey(Activity, related_name='tags')
+    # REQUIRED
+    description = models.CharField(max_length=20)
+    activity = models.ForeignKey(Activity, related_name='tags')
 
-	def __unicode__(self):
-		return self.description
-
-
-
+    def __unicode__(self):
+        return self.description
